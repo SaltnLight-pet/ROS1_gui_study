1f798509545dc3ba4ecb7c707fdaf6b1c2db3385
parkeuntaek <pet.saltnlight@gmail.com>
2023-09-06 22:23:28 +0900

    remain ws

--- kw_gui/setup.py
+++ kw_gui/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
-
+  
 d = generate_distutils_setup(
-    packages=['my_gui'],
-    package_dir={'':'src'}
+    packages=['kw_gui'],
+    package_dir={'': 'src'},
 )
 
 setup(**d)
